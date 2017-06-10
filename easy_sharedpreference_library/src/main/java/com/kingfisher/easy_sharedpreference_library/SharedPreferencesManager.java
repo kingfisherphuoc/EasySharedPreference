28407e44c40e6498038a66c246518a22b7a2f481
kingfisher <phuocdh53@gmail.com>
2017-06-10 12:35:44 +0700

    First commit

---
+++ easy_sharedpreference_library/src/main/java/com/kingfisher/easy_sharedpreference_library/SharedPreferencesManager.java
@@ -0,0 +1,165 @@
+package com.kingfisher.easy_sharedpreference_library;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by kingfisher on 6/10/17.
+ */
+
+public class SharedPreferencesManager {
+
+    private static final String DEFAULT_SHARED_PREFERENCE = "MySharedPreference";
+    private static HashMap<String, SharedPreferencesManager> preferenceManagerHashMap = new HashMap<>();
+    private SharedPreferences sharedPreference;
+    private SharedPreferences.Editor editor;
+    private Gson gson;
+
+    private static SharedPreferencesManager getSharedPreferenceManager(Context context, String sharedPreferenceName) {
+        if (preferenceManagerHashMap == null) preferenceManagerHashMap = new HashMap<>();
+        // get or create new sharedPreferenceManager
+        String name = TextUtils.isEmpty(sharedPreferenceName) ? DEFAULT_SHARED_PREFERENCE : sharedPreferenceName;
+        SharedPreferencesManager sharedPreferencesManager = preferenceManagerHashMap.get(name);
+        if (sharedPreferencesManager == null) {
+            sharedPreferencesManager = new SharedPreferencesManager(context, name);
+            preferenceManagerHashMap.put(name, sharedPreferencesManager);
+        }
+        return sharedPreferencesManager;
+    }
+
+
+    private SharedPreferencesManager(Context context, String name) {
+        sharedPreference = context.getApplicationContext().getSharedPreferences(name, Context.MODE_PRIVATE);
+        editor = sharedPreference.edit();
+        gson = new Gson();
+    }
+
+    /**
+     * You have to initialize all your preference when open app.
+     *
+     * @param context
+     * @param createDefaultPreference : true if we want to create default {@link SharedPreferences}
+     * @param names                   : list of custom {@link SharedPreferences}
+     */
+    public static synchronized void init(Context context, boolean createDefaultPreference, String... names) {
+        if (createDefaultPreference) {
+            getSharedPreferenceManager(context, DEFAULT_SHARED_PREFERENCE);
+        }
+        if (names == null || names.length == 0) return;
+        for (String name : names) {
+            getSharedPreferenceManager(context, name);
+        }
+    }
+
+    /**
+     * Get {@link SharedPreferences} with name: name in {@link Context#MODE_PRIVATE}.
+     *
+     * @param name
+     * @return
+     */
+    public static synchronized SharedPreferencesManager getInstance(String name) {
+        SharedPreferencesManager sharedPreferencesManager = preferenceManagerHashMap.get(name);
+        if (sharedPreferencesManager == null)
+            throw new IllegalStateException("The share preference: " + name + " is not initialized before. You have to initialize it first by calling init(Context, boolean, String...) function");
+        return sharedPreferencesManager;
+    }
+
+    /**
+     * Get default sharedPreference name: {@link #DEFAULT_SHARED_PREFERENCE}
+     *
+     * @return
+     */
+    public static synchronized SharedPreferencesManager getInstance() {
+        SharedPreferencesManager sharedPreferencesManager = preferenceManagerHashMap.get(DEFAULT_SHARED_PREFERENCE);
+        if (sharedPreferencesManager == null)
+            throw new IllegalStateException("The default share preference is not initialized before. You have to initialize it first by calling init(Context, boolean, String...) function");
+        return sharedPreferencesManager;
+    }
+
+    /**
+     * Easy to save your value here.
+     *
+     * @param key   : the key you want to save the object with
+     * @param value : can be any kind of object (int, long, boolean, string, float, YourConvertableObject)
+     * @return
+     */
+    public SharedPreferencesManager putValue(String key, Object value) {
+
+        if (value instanceof Integer) {
+            editor.putInt(key, (Integer) value);
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) value);
+        } else if (value instanceof Float) {
+            editor.putFloat(key, (Float) value);
+        } else if (value instanceof String) {
+            editor.putString(key, (String) value);
+        } else if (value instanceof Long) {
+            editor.putLong(key, (Long) value);
+        } else {
+            String json = gson.toJson(value);
+            editor.putString(key, json);
+        }
+        editor.commit();
+        return this;
+    }
+
+    /**
+     * Get value which is saved
+     *
+     * @param key
+     * @param type
+     * @param <T>
+     * @return
+     */
+    public <T> T getValue(String key, Class<T> type) {
+        if (type == Integer.class) {
+            Integer value = sharedPreference.getInt(key, 0);
+            return (T) value;
+        } else if (type == Boolean.class) {
+            Boolean value = sharedPreference.getBoolean(key, false);
+            return (T) value;
+        } else if (type == Float.class) {
+            Float value = sharedPreference.getFloat(key, 0);
+            return (T) value;
+        } else if (type == String.class) {
+            String value = sharedPreference.getString(key, "");
+            return (T) value;
+        } else if (type == Long.class) {
+            Long value = sharedPreference.getLong(key, 0);
+            return (T) value;
+        } else {
+            String json = sharedPreference.getString(key, "");
+            if (TextUtils.isEmpty(json)) {
+                return null;
+            } else {
+                T value = gson.fromJson(json, type);
+                return value;
+            }
+        }
+    }
+
+    public <T> List<T> getValues(String key, Class<T[]> clazz) {
+        String json = sharedPreference.getString(key, "");
+        if (TextUtils.isEmpty(json)) return null;
+        T[] values = gson.fromJson(json, clazz);
+        List<T> list = new ArrayList<T>();
+        list.addAll(Arrays.asList(values));
+        return list;
+    }
+
+
+    private static void showLog(String s) {
+        Log.e(SharedPreferencesManager.class.getCanonicalName(), s);
+    }
+
+
+}
---
+++ sample/src/main/java/com/kingfisher/sample/MainActivity.java
@@ -0,0 +1,107 @@
+package com.kingfisher.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.kingfisher.easy_sharedpreference_library.SharedPreferencesManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static String AnotherPreferenceName = "my_test_preference";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferencesManager.init(this, true, AnotherPreferenceName);
+
+        findViewById(R.id.btnSave).setOnClickListener(this);
+        findViewById(R.id.btnLoadData).setOnClickListener(this);
+    }
+
+    private void loadData() {
+        showLog("Get stored value: " + SharedPreferencesManager.getInstance().getValue("Test", Integer.class));
+        showLog("Test first: " + SharedPreferencesManager.getInstance().getValue("testData", Gun.class));
+        showLog("Person: " + SharedPreferencesManager.getInstance().getValue("person", Person.class));
+        showLog("Fruits: " + SharedPreferencesManager.getInstance(AnotherPreferenceName).getValues("fruits", String[].class));
+        List<Gun> datas = SharedPreferencesManager.getInstance(AnotherPreferenceName).getValues("guns", Gun[].class);
+        if (datas == null) return;
+        for (Gun gun1 : datas) {
+            showLog(gun1.toString());
+        }
+    }
+
+    private void saveData() {
+        SharedPreferencesManager.getInstance().putValue("Test", 1);
+        Gun testGun = new Gun("test first!", 15);
+        Gun gun = new Gun("a boss with a gun!", 2);
+        SharedPreferencesManager.getInstance().putValue("testData", testGun);
+        SharedPreferencesManager.getInstance().putValue("person", new Person(gun, true, "Hell Boy"));
+        // using another shared preference
+        SharedPreferencesManager.getInstance(AnotherPreferenceName).putValue("fruits",
+                Arrays.asList(new String[]{"banana", "apple", "bean"}));
+        List<Gun> guns = Arrays.asList(new Gun[]{gun, testGun});
+        SharedPreferencesManager.getInstance(AnotherPreferenceName).putValue("guns", guns);
+
+    }
+
+    public void showLog(String mess) {
+        Log.e("Main", mess);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSave:
+                saveData();
+                break;
+            case R.id.btnLoadData:
+                loadData();
+                break;
+        }
+    }
+
+    public static class Gun {
+        public String des;
+        public int id;
+
+        public Gun(String des, int id) {
+            this.des = des;
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "TestData{" +
+                    "des='" + des + '\'' +
+                    ", id=" + id +
+                    '}';
+        }
+    }
+
+    public static class Person {
+        public Gun gun;
+        public boolean hasGun;
+        public String name;
+
+        public Person(Gun gun, boolean hasGun, String name) {
+            this.gun = gun;
+            this.hasGun = hasGun;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "testData=" + gun +
+                    ", hasGun=" + hasGun +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
